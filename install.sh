3d0e386efe237a83f045d54ada36fadc2b204f05
Sergey Panov <gostroy@gmail.com>
2013-06-19 16:51:28 +0300

    modified:   etc/percona-toolkit/pt-kill.conf 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
+
 rsync -va /usr/src/git/github/pt-init-scripts/etc /
+
+mysql --defaults-extra-file=/root/.my.cnf -e "create database percona;"
 
 echo ""
 echo "chkconfig --list|grep "^pt""
